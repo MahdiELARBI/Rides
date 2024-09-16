c33760db05ed285f9eca12968d8e6223226cc253
MahdiELARBI <mahdi.arbi.esprit@gmail.com>
2024-09-16 16:01:22 -0400

    Initialize project

---
+++ Rides/Rides/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rides
+//
+//  Created by Mahdi Elarbi on 2024-09-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rides/Rides/RidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RidesApp.swift
+//  Rides
+//
+//  Created by Mahdi Elarbi on 2024-09-16.
+//
+
+import SwiftUI
+
+@main
+struct RidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
